da7612271201acbde32e619b9820c24d2481a528
Jonathon Gonzalez <jonnygabc@hotmail.com>
2024-01-29 22:47:12 -0800

    adding all new changes to demo page

--- src/components/section/DemoQuestion.jsx
+++ src/components/section/DemoQuestion.jsx
@@ -1,44 +1,66 @@
-import React from "react";
-import CheckedInput from "../inputs/CheckedInput";
+import React from 'react';
+import CheckedInput from '../inputs/CheckedInput';
 
-const DemoQuestion = ({story, handleOption, answer}) => {
+const DemoQuestion = ({ story, answer, handleAnswer }) => {
+    if (!story || typeof story !== 'object') {
+        // Handle the case where story is not available or has an unexpected format
+        return <div>Loading Story...</div>;
+    }
 
-  return (
-
-        <div className='grid grid-rows-2 col-span-1 max-h-[80vh]  border-2 border-zinc-800'>
-            <div className="row-span-1 text-xs p-1.5">
-                <span>{story.QuestionPrompt[0]}</span>
-                <br/>
-                <br/>
-                <span>{story.QuestionPrompt[1]}</span>
+    return (
+        <div className="grid grid-rows-2 col-span-1 border-2 gap-5 border-zinc-800 overflow-auto pb-10 h-[74.25vh]">
+            <div className="row-span-1 pb-4 text-xs p-1.5 overflow-auto">
+                <h1>Question:</h1>
+                <span>{story.question}</span>
             </div>
-            <div className="grid grid-rows-3 row-span-1 p-1.5">
-              <div>
-                <span className="font-bold pr-1.5">Decision:</span>
-                <span>{story.Question}</span>
-              </div>
+            {story.questionTypeId === 1 && (
+                <div className="grid mt-10 row-span-1 p-1.5 ">
+                    <div className="grid row-span-2 ">
+                        <div>
+                            <CheckedInput
+                                checked={answer === 1}
+                                onChange={() => handleAnswer(1)}
+                                option={story.qoption1}
+                            />
+                            <span className="pl-1.5">{story.qoption1}</span>
+                        </div>
+                        <div className="">
+                            <CheckedInput
+                                checked={answer === 2}
+                                onChange={() => handleAnswer(2)}
+                                option={story.qoption2}
+                            />
 
-              <div className="grid row-span-2">
-                <div>
-                <CheckedInput
-                 checked={answer === story.Qoption1}
-                 onChange={() => handleOption(story.Qoption1)}
-                option={story.Qoption1}/>
-                  <span className="pl-1.5">{story.Qoption1}</span>
+                            <span className="pl-1.5">{story.qoption2}</span>
+                        </div>
+                    </div>
                 </div>
-                <div className="">
-                <CheckedInput
-                 checked={answer === story.Qoption2}
-                 onChange={() => handleOption(story.Qoption2)}
-                option={story.Qoption1}/>
-                <span className="pl-1.5">{story.Qoption2}</span>
+            )}
+            {story.questionTypeId === 3 && (
+                <div className="grid mb-10 pb-10 row-span-1 p-1.5">
+                    <div className="grid grid-rows-11 text-[10px] text-center font-bold mx-2">
+                        <h1 className="text-lg ">Select an Answer</h1>
+                        {Array.from({ length: 10 }, (_, index) => (
+                            <div
+                                key={index}
+                                className="grid grid-cols-10 w-full">
+                                <span className="text-center col-span-1 p-0.5 rounded-sm my-0.5 font-extrabold">
+                                    {`${index + 1}.`}
+                                </span>
+                                <div
+                                    onClick={() =>
+                                        parseInt(handleAnswer(index + 1))
+                                    }
+                                    className="col-span-9 border-[1px] bg-[#0C7513] text-white my-0.5 border-black p-0.5 rounded-md hover:scale-105 hover:cursor-pointer">
+                                    {story[`qoption${index + 1}`]}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                
-              </div>
-            </div>
+            )}
         </div>
- 
-  );
-}
+    );
+};
 
 export default DemoQuestion;
--- src/views/StoryProblems.jsx
+++ src/views/StoryProblems.jsx
@@ -1,32 +1,81 @@
-import React, { useState } from "react";
-import DemoQuestion from "../components/section/DemoQuestion";
-import DemoStory from "../components/section/DemoStory";
-import { Story } from "../constants";
+import React, { useState } from 'react';
+import DemoQuestion from '../components/section/DemoQuestion';
+import DemoStory from '../components/section/DemoStory';
+import services from '../services/services';
+import { useEffect } from 'react';
 //import SimpleChatPrompt from "../components/section/AiChatbot";
-
+import { Story } from '../constants';
 const StoryProblems = () => {
-  const [selectedOption, setSelectedOption] = useState(null);
+    const [allStories, setAllStories] = useState([]);
+    const [storyStep, setStoryStep] = useState(0);
+    const [answer, setAnswer] = useState(null);
+    useEffect(() => {
+        const fetchAll = async () => {
+            try {
+                // Assuming services.storyLines() returns a promise
+                await services
+                    .fullStory()
+                    .then(allStoryLinesSuccess)
+                    .catch(allStoryLinesError);
+            } catch (error) {
+                console.error('Error fetching story lines:', error);
+            }
+        };
 
-  const handleOptionChange = (option) => {
-    setSelectedOption(option);
-  };
+        fetchAll();
+    }, []);
+    const handleAnswer = (answer) => {
+        setAnswer(answer);
+    };
 
-  return (
-    <div className="grid grid-cols-4 mt-1 max-w-[65rem] mx-auto h-[75vh] w-full p-2 gap-x-1 overflow-auto">
-      {/*story-line*/}
-      <DemoStory story={Story[0]} />
-      {/*questionaire*/}
-      <DemoQuestion
-        handleOption={handleOptionChange}
-        answer={selectedOption}
-        story={Story[0]}
-      />
-      {/* ai chat bot
+    const allStoryLinesSuccess = (result) => {
+        let newResult = result.filter((story) => {
+            return story.storyId === 1;
+        });
+        setAllStories(newResult);
+    };
+
+    const allStoryLinesError = (errr) => {
+        console.log(errr);
+    };
+
+    useEffect(() => {
+        const nextStep = () => {
+            const currentStory = allStories[storyStep];
+
+            if (
+                currentStory &&
+                currentStory.correctAnswerOption === parseInt(answer)
+            ) {
+                if (storyStep < allStories.length - 1) {
+                    setStoryStep((prev) => prev + 1);
+                } else {
+                    setStoryStep(1);
+                }
+            } else {
+                // Handle the case where the answer is incorrect
+            }
+        };
+
+        nextStep();
+    }, [answer, storyStep, allStories]);
+    console.log(allStories, storyStep);
+    return (
+        <div className="grid grid-cols-4 mt-1 max-w-[80rem] mx-auto h-[75vh] w-full m-20 p-12 gap-x-1">
+            {/*story-line*/}
+            <DemoStory story={allStories[storyStep]} />
+            {/*questionaire*/}
+            <DemoQuestion
+                answer={answer}
+                story={allStories[storyStep]}
+                handleAnswer={handleAnswer}
+            />
+            {/* ai chat bot
       <div className="grid col-span-1 h-[75vh] max-h-[75vh] border-2 border-zinc-800">
         <SimpleChatPrompt />
       </div> */}
-    </div>
-  );
+        </div>
+    );
 };
 
 export default StoryProblems;
